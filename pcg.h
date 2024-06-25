7ad43e590b604630eb157f93a7923e9a3a89dba9
Tony Finch <dot@dotat.at>
2024-06-25 15:15:54 +0100

    never enter the slow path when the limit is a power of two

--- pcg.h
+++ pcg.h
@@ -39,7 +39,7 @@ extern pcg_uint_t pcg_rand_slow(
 static inline pcg_uint_t
 pcg_rand(pcg_t *rng, pcg_uint_t limit) {
 	pcg_ulong_t hi_lo = (pcg_ulong_t)pcg_random(rng) * (pcg_ulong_t)limit;
-	if ((pcg_uint_t)(hi_lo) < limit)
+	if ((limit & (limit - 1)) > 0 && (pcg_uint_t)(hi_lo) < limit)
 		return (pcg_rand_slow(rng, limit, hi_lo));
 	return ((pcg_uint_t)(hi_lo >> PCG_UINT_BITS));
 }
