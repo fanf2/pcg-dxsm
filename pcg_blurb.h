bf43bbb451e3db170dbfcfc73c7e3d953cf2371c
Tony Finch <dot@dotat.at>
2024-06-27 15:37:33 +0100

    appease gcc

--- pcg_blurb.h
+++ pcg_blurb.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
-#define pcg_rand_fast(rng, limit)				 \
-	pcg_rand_inline(rng, limit,				  \
-		_Generic(0 ? (void *)(long)((limit) & (limit) - 1) \
+#define pcg_rand_fast(rng, limit)				   \
+	pcg_rand_inline(rng, limit,				    \
+		_Generic(0 ? (void *)(long)((limit) & ((limit) - 1)) \
 			   : (long *) 0, long *: 0, void *: 1))
 
